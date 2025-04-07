090e125c81262927da1ea9d3e607e91f4d53e12a
Moein Alemi nia <moien.alemi.nia@gmail.com>
2025-04-07 20:51:25 +0330

    add Entity's examples

---
+++ src/example/Human.java
@@ -0,0 +1,21 @@
+package example;
+
+import db.*;
+
+public class Human extends Entity {
+    public String name;
+    public int age;
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public Human copy() {
+        Human copyHuman = new Human(this.name, this.age);
+        copyHuman.id = id;
+
+        return copyHuman;
+    }
+}
