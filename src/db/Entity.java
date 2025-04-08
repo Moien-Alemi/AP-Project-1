5f94f20026f82a523a797b17fbec0114dbad6b36
Moein Alemi nia <moien.alemi.nia@gmail.com>
2025-04-08 12:46:53 +0330

    add tracking date and time of creation and last modification feature

--- src/db/Entity.java
+++ src/db/Entity.java
@@ -1,7 +1,11 @@
 package db;
 
+import java.util.Date;
+
 public abstract class Entity {
     public int id;
+    protected Date creationDate;
+    protected Date lastModificationDate;
 
     public abstract Entity copy();
 
