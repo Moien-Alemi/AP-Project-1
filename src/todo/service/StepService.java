6fe450c662ee424a865e6324eb7da85ba71c2ed1
Moein Alemi nia <moien.alemi.nia@gmail.com>
2025-04-13 23:53:57 +0330

    update services

--- src/todo/service/StepService.java
+++ src/todo/service/StepService.java
@@ -41,6 +41,7 @@ public class StepService {
             switch (field) {
                 case "title":
                     newStep = new Step(newValue, oldStep.getStatus(), oldStep.getTaskRef());
+                    newStep.id = oldStep.id;
                     Database.update(newStep);
                     System.out.println("Successfully updated the Step.");
                     System.out.println("Field: " + field);
@@ -50,10 +51,11 @@ public class StepService {
                     System.out.println();
                     break;
                 case "status":
-                    newValue = newValue.toUpperCase();
                     Step.Status newStatus = Step.Status.valueOf(newValue);
                     newStep = new Step(oldStep.getTitle(), newStatus, oldStep.getTaskRef());
+                    newStep.id = oldStep.id;
                     Database.update(newStep);
+                    StepService.checkTaskStatus(newStep.getTaskRef());
                     System.out.println("Successfully updated the Step.");
                     System.out.println("Field: " + field);
                     System.out.println("Old value: " + oldStep.getStatus());
@@ -75,5 +77,49 @@ public class StepService {
         }
     }
 
-}
+    public static void checkTaskStatus(int taskID){
+        ArrayList<Step> stepList = findAllSteps(taskID);
+
+        boolean isNotStarted = true;
+        boolean isComplete = true;
+        for (Step s : stepList) {
+            if (s.getStatus() == Step.Status.Completed) {
+                isNotStarted = false;
+                break;
+            }
+        }
+        if (isNotStarted == false) {
+            for(Step s : stepList) {
+                if (s.getStatus() != Step.Status.Completed){
+                    isComplete = false;
+                    break;
+                }
+            }
+        }
+
+        if (isNotStarted == false ){
+            if(isComplete == true){
+                TaskService.updateTask(taskID, "status", "Completed");
+            }else {
+                TaskService.updateTask(taskID, "status", "InProgress");
+            }
+        }
+    }
+
+    public static ArrayList<Step> findAllSteps(int taskID){
+        ArrayList<Step> stepList = new ArrayList<>();
+        for (Entity entity : Database.getAll(Step.STEP_ENTITY_CODE)) {
+            if (((Step) entity).getTaskRef() == taskID) {
+                stepList.add(((Step) entity));
+            }
+        }
+        return stepList;
+    }
+
+    public static void showStep(Step step){
+        System.out.println("\t+ " + step.getTitle() + ":");
+        System.out.println("\t\tID: " + step.id);
+        System.out.println("\t\tStatus: " + step.getStatus());
+    }
+
 }
--- src/todo/service/TaskService.java
+++ src/todo/service/TaskService.java
@@ -2,13 +2,13 @@ package todo.service;
 
 import db.Database;
 import db.Entity;
-import db.exception.EntityNotFoundException;
-import db.exception.InvalidEntityException;
 import todo.entity.Step;
 import todo.entity.Task;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Date;
 
 
@@ -36,10 +36,8 @@ public class TaskService {
             Entity entity = Database.get(id);
             if(entity instanceof Task){
                 Database.delete(id);
-                for(Entity taskStep : Database.getAll(Step.STEP_ENTITY_CODE)) {
-                    if (((Step) taskStep).getTaskRef() == id) {
-                        Database.delete(taskStep.id);
-                    }
+                for(Entity taskStep : StepService.findAllSteps(id)) {
+                    Database.delete(taskStep.id);
                 }
                 System.out.println("Entity with ID = " + id + " successfully deleted.");
             }
@@ -60,6 +58,9 @@ public class TaskService {
             switch (field) {
                 case "title":
                     newTask = new Task(newValue, oldTask.getDescription(), oldTask.getDueDate(), oldTask.getStatus());
+                    newTask.id = oldTask.id;
+                    newTask.setCreationDate(oldTask.getCreationDate());
+                    newTask.setLastModificationDate(oldTask.getLastModificationDate());
                     Database.update(newTask);
                     System.out.println("Successfully updated the task.");
                     System.out.println("Field: " + field);
@@ -70,6 +71,9 @@ public class TaskService {
                     break;
                 case "description":
                     newTask = new Task(oldTask.getTitle(), newValue, oldTask.getDueDate(), oldTask.getStatus());
+                    newTask.id = oldTask.id;
+                    newTask.setCreationDate(oldTask.getCreationDate());
+                    newTask.setLastModificationDate(oldTask.getLastModificationDate());
                     Database.update(newTask);
                     System.out.println("Successfully updated the task.");
                     System.out.println("Field: " + field);
@@ -82,6 +86,9 @@ public class TaskService {
                     SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
                     Date newDueDate = format.parse(newValue);
                     newTask = new Task(oldTask.getTitle(), oldTask.getDescription(), newDueDate, oldTask.getStatus());
+                    newTask.id = oldTask.id;
+                    newTask.setCreationDate(oldTask.getCreationDate());
+                    newTask.setLastModificationDate(oldTask.getLastModificationDate());
                     Database.update(newTask);
                     System.out.println("Successfully updated the task.");
                     System.out.println("Field: " + field);
@@ -91,11 +98,13 @@ public class TaskService {
                     System.out.println();
                     break;
                 case "status":
-                    newValue = newValue.toUpperCase();
                     Task.Status newStatus = Task.Status.valueOf(newValue);
                     newTask = new Task(oldTask.getTitle(), oldTask.getDescription(), oldTask.getDueDate(), newStatus);
+                    newTask.id = oldTask.id;
+                    newTask.setCreationDate(oldTask.getCreationDate());
+                    newTask.setLastModificationDate(oldTask.getLastModificationDate());
                     Database.update(newTask);
-                    checkTaskStatus(newTask);
+                    TaskService.checkTaskStatus(newTask);
                     System.out.println("Successfully updated the task.");
                     System.out.println("Field: " + field);
                     System.out.println("Old value: " + oldTask.getStatus());
@@ -122,11 +131,66 @@ public class TaskService {
 
     public static void checkTaskStatus(Task task){
         if(task.getStatus() == Task.Status.Completed){
-            for(Entity entity : Database.getAll(Step.STEP_ENTITY_CODE)){
-                if(((Step) entity).getTaskRef() == task.id){
+            for(Step entity : StepService.findAllSteps(task.id)){
+                if (entity.getStatus() != Step.Status.Completed) {
                     StepService.updateStep(entity.id, "status", "Completed");
                 }
             }
+        }
+    }
+
+    public static void getTask(int ID){
+        try {
+            Task task = (Task) Database.get(ID);
+            showTask(task);
+        } catch (Exception e) {
+            System.out.println("Can not get task.");
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void showTask(Task task){
+        System.out.println();
+        System.out.println("ID: " + task.id);
+        System.out.println("Title: " + task.getTitle());
+        System.out.println("Due Date: " + task.getDueDate());
+        System.out.println("Status: " + task.getStatus());
+        ArrayList<Step> stepList = StepService.findAllSteps(task.id);
+        if(stepList.size() != 0){
+            System.out.println("Steps:");
+            for(Step step : stepList){
+                StepService.showStep(step);
+            }
+        }
+        System.out.println();
+
+    }
+
+    public static void getAllTask(){
+        ArrayList<Task> allTasks = new ArrayList<>();
+        for (Entity entity : Database.getAll(Task.TASK_ENTITY_CODE)){
+            allTasks.add((Task) entity);
+        }
+
+        allTasks.sort(Comparator.comparing(Task :: getDueDate));
+
+        for (Task task : allTasks){
+            showTask(task);
+        }
+    }
+
+    public static void getIncompleteTasks(){
+        ArrayList<Task> incompleteTasks = new ArrayList<>();
+        for (Entity entity : Database.getAll(Task.TASK_ENTITY_CODE)){
+            if (((Task) entity).getStatus() != Task.Status.Completed) {
+                incompleteTasks.add((Task) entity);
+            }
+        }
+
+        incompleteTasks.sort(Comparator.comparing(Task :: getDueDate));
+
+        for (Task task : incompleteTasks){
+            showTask(task);
         }
     }
 }
